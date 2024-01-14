f65247ea60e681774bbda7980a5311b6cb9a5ac4
Erick Mutua <108677764+Mechantchulo@users.noreply.github.com>
2024-01-14 21:36:47 +0300

    Update area.c

--- area.c
+++ area.c
@@ -2,11 +2,14 @@
 //AREA of X and Y = A
 
 #include<stdio.h>
+
+//create variables of the integres to use in the program
 int main(){
 int X = 32;
 int Y = 40;
 int A = X * Y;
 
+  //display the result of the area
 printf("Area of X and Y = A:%d\n",A);
 return 0;
 
