d2e68fd72dae3821221ecfac6aa6520e27395603
Erick Mutua <108677764+Mechantchulo@users.noreply.github.com>
2024-01-14 21:39:25 +0300

    Update area2.c

--- area2.c
+++ area2.c
@@ -7,19 +7,19 @@ int main(){
     int X;
     int Y;
     int A; 
-
+//prompt the user to input the value of x
     printf("\nENTER X:");
     scanf("%d", &X);
-
+//prompt the user to input the value of Y
     printf("\nENTER Y:");
     scanf("%d", &Y);
-
+//display the result of the multiplication of y and x
     printf("RESULT A:",A = X * Y);
     
 
     printf("\n");
     printf("\n");
-
+//display the results of the above programs 
     printf("ENTER X:%d\n",X);
     printf("ENTER Y:%d\n",Y);
     printf("RESULT A:%d\n",A); 
